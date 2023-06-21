1437d16d2275a3d896dd8bcd6cb948f64a9092d4
Fernando_117 <fernandogijon@outlook.es>
2023-06-21 17:28:45 -0600

    Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+
+const Description = ({text}) =>{
+  return <p>{text}</p>
+}
+
+const Title = ({ text, color}) =>{
+  return <h1 style={{color}}>{text}</h1>
+}
+
+
+
+
+const Card = () => {
+
+  const onHandlerClick = (name) =>{
+    console.log('click' , name)
+  }
+
+  return (
+  <div>
+  <Title text='Bienvenidos' color="pink"/>
+  <Description text='Esta es una descripcion'/>
+  <Button onHandlerClick={onHandlerClick} color='pink' name='Clickeame'/>
+  </div>
+  )
+}
+
+const Button = ({onHandlerClick, name , color}) =>{
+  return (
+  <button type = 'button' onClick={() => onHandlerClick(name)} style={{background: color}}>{name}</button>
+    )
+}
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,19 +51,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Card/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </>
   )
 }
 
